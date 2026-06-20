92edc5b240220531129a25fb9ef4a3503ec7e13e
AlejoGDOfficial <alejogdoff@gmail.com>
2026-06-19 21:15:18 -0500

    tung

--- source/InitMacros.hx
+++ source/InitMacros.hx
@@ -2,11 +2,20 @@ package;
 
 import util.CPPExternMacro;
 
+import sys.io.File;
+
+import haxe.Json;
+
+typedef ExternsConfig = {
+    path:String,
+    classes:Array<TypeConfig>
+}
+
 class InitMacros
 {
     public static function main()
     {
-        final config:{path:String, classes:Array<TypeConfig>} = cast haxe.Json.parse(sys.io.File.getContent('externsConfig.json'));
+        final config:ExternsConfig = cast Json.parse(File.getContent('externsConfig.json'));
 
         CPPExternMacro.PATH = config.path;
 
--- source/Main.hx
+++ source/Main.hx
@@ -4,8 +4,6 @@ class Main
 {
     static function main()
     {
-        trace('tung');
-
         Extern.example('tung');
     }
 }
